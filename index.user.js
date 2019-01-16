b59895ab1fed6f23c81638f8535b50b8fe66706e
Giuseppe <giuseppeg@users.noreply.github.com>
2019-01-16 11:06:32 +0100

    0.2.0

--- index.user.js
+++ index.user.js
@@ -1,34 +1,13 @@
 // ==UserScript==
 // @name refined-twitter-lite
 // @description Small UserScript that adds some UI improvements to Twitter Lite
-// @version 0.1.0
+// @version 0.2.0
 // @match https://mobile.twitter.com/*
 // ==/UserScript==
 document.head.insertAdjacentHTML('beforeend', `
   <style>
-    main > div > div > div {
-      flex-direction: row-reverse !important
-    }
     [data-testid="sidebarColumn"] {
-      margin: 0 !important;
-    }
-    [data-testid="sidebarColumn"] > div > div {
-      width: 320px !important;
-    }
-    [data-testid="primaryColumn"],
-    [data-testid="primaryColumn"]  > div > div {
-      max-width: 640px !important;
-    }
-    [data-testid="fab-tweet"] {
-      right: -299px !important;
-      width: 80px !important;
-      height: 40px !important;
-    }
-    [data-testid="tweet"] [lang] {
-      opacity: 0.9 !important;
-      line-height: 23px !important;
-      font-size: 16px !important;
-      font-family: "Helvetica Neue",Helvetica,Arial,sans-serif !important;
+      display: none;
     }
   </style>
 `)
