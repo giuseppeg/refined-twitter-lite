8803d9df8ca9267c820fcc7488c5192a84964b31
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2019-01-17 09:18:09 +0100

    0.2.0

--- index.user.js
+++ index.user.js
@@ -2,12 +2,15 @@
 // @name refined-twitter-lite
 // @description Small UserScript that adds some UI improvements to Twitter Lite
 // @version 0.2.0
-// @match https://mobile.twitter.com/*
+// @match https://*.twitter.com/*
 // ==/UserScript==
 document.head.insertAdjacentHTML('beforeend', `
   <style>
     [data-testid="sidebarColumn"] {
       display: none;
+    }
+    .rn-1ye8kvj {
+      min-width: 678px;
     }
   </style>
 `)
