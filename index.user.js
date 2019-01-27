463f83a0644e695e0f14585f79771b58487c80bb
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2019-01-27 14:06:43 +0100

    whatev

--- index.user.js
+++ index.user.js
@@ -3,7 +3,6 @@
 // @description Small UserScript that adds some UI improvements to Twitter Lite
 // @version 0.2.1
 // @match https://*.twitter.com/*
-// @run-at document-start
 // @grant none
 // ==/UserScript==
 (function () {
@@ -17,41 +16,32 @@
     document.documentElement.setAttribute('data-refined-twitter-lite', features.join(' '))
   }
 
-  function onNavigate(state, title, url) {
+  function onNavigate(url) {
     const parsedUrl = document.createElement('a')
-    parsedUrl.href = url || window.location.href
+    parsedUrl.href = url
     setFeatures(
-      Object.keys(features).filter(feature => features[feature](parsedUrl, title || ''))
+      Object.keys(features).filter(feature => features[feature](parsedUrl, document.title || ''))
     )
   }
 
-  const pushState = history.pushState
-  history.pushState = function pushState() {
-    if (typeof history.onpushstate == "function") {
-      history.onpushstate.apply(history, arguments)
+  let currentUrl = window.location.href
+  document.documentElement.addEventListener('DOMNodeInserted', () => {
+    if (window.location.href === currentUrl) {
+      return
     }
-
-    return pushState.apply(history, arguments)
-  }
-
-  history.onpushstate = function () {
-    console.log('onNavigate')
-    onNavigate.apply(null, arguments)
-  }
-
-  window.addEventListener('load', () => {
-    window.addEventListener('popstate', onNavigate)
-    onNavigate(null, document.title, window.location.href)
-
-    document.head.insertAdjacentHTML('beforeend', `
-      <style>
-        [data-refined-twitter-lite~="singleColumn"] [data-testid="sidebarColumn"] {
-          display: none;
-        }
-        [data-refined-twitter-lite~="singleColumn"] .rn-1ye8kvj {
-          min-width: 678px;
-        }
-      </style>
-    `)
+    currentUrl = window.location.href
+    onNavigate(currentUrl)
   })
+  onNavigate(currentUrl)
+
+  document.head.insertAdjacentHTML('beforeend', `
+    <style>
+      [data-refined-twitter-lite~="singleColumn"] [data-testid="sidebarColumn"] {
+        display: none;
+      }
+      [data-refined-twitter-lite~="singleColumn"] .rn-1ye8kvj {
+        min-width: 678px;
+      }
+    </style>
+  `)
 }())
