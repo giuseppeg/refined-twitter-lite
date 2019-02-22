4c67cd9b09114ae86f1966c5aea4aaa980644f6c
Giuseppe Gurgone <giuseppeg@users.noreply.github.com>
2019-02-22 16:34:55 +0100

    Add more features

--- index.user.js
+++ index.user.js
@@ -6,42 +6,132 @@
 // @grant none
 // ==/UserScript==
 (function () {
+  let prevUrl = window.location.href
   const features = {
-    singleColumn: (parsedUrl, title) => {
-      return /^((?!messages|settings).)*$/.test(parsedUrl.pathname)
+    singleColumn: {
+      default: true,
+      test: (parsedUrl, title) => {
+        return /^((?!messages|settings).)*$/.test(parsedUrl.pathname)
+      },
+      styles: [
+        '[data-testid="sidebarColumn"] { display: none }',
+        '.rn-1ye8kvj { min-width: 678px }'
+      ]
+    },
+    composeButtonTextCursor: {
+      default: true,
+      styles: [
+        `[href="/compose/tweet"] [dir] { cursor: text }`
+      ]
+    },
+    hideLikeCount: {
+      default: true,
+      styles: [
+        `[href$="/likes"],
+         [data-testid="like"] span,
+         [data-testid="unlike"] span {
+            display: none
+         }`
+      ]
+    },
+    hideRetweetCount: {
+      default: true,
+      styles: [
+        `[href$="/retweets"],
+         [data-testid="retweet"] span,
+         [data-testid="unretweet"] span {
+            display: none
+         }`
+      ]
+    },
+    hideReplyCount: {
+      default: true,
+      styles: [
+        `[data-testid="reply"] span { display: none }`
+      ]
+    },
+    hideAvatars: {
+      default: true,
+      styles: [
+        `[style*="/profile_images/"] { display: none }`
+      ]
+    },
+    obfuscateName: {
+      default: true,
+      styles: [
+        `[data-testid="tweet"] [href^="/"] span[dir] {
+            background-color: currentColor;
+            border-radius: 3px;
+            opacity: 0.9;
+         }
+        `
+      ]
+    },
+    hideName: {
+      default: false,
+      styles: [
+        `[data-testid="tweet"] [href^="/"] span[dir] {
+            display: none
+         }
+        `
+      ]
     }
   }
 
-  function setFeatures(features) {
-    document.documentElement.setAttribute('data-refined-twitter-lite', features.join(' '))
-  }
+  const storageKey = 'refined-twitter-lite'
+  let settings = {}
+  const storedSettings = JSON.parse(localStorage.getItem(storageKey)) || {}
+  settings = Object.keys(features).reduce((settings, feature) => {
+    if (typeof storedSettings[feature] === 'boolean') {
+      settings[feature] = storedSettings[feature]
+    } else {
+      settings[feature] = features[feature].default
+    }
+    return settings
+  }, {})
+  console.log(storedSettings)
 
-  function onNavigate(url) {
+  function setFeatures() {
     const parsedUrl = document.createElement('a')
-    parsedUrl.href = url
-    setFeatures(
-      Object.keys(features).filter(feature => features[feature](parsedUrl, document.title || ''))
-    )
-  }
+    parsedUrl.href = window.location.href
 
-  let currentUrl = window.location.href
-  document.body.addEventListener('DOMNodeInserted', () => {
-    if (window.location.href === currentUrl) {
-      return
-    }
-    currentUrl = window.location.href
-    onNavigate(currentUrl)
-  })
-  onNavigate(currentUrl)
+    const enabledFeatures = Object.keys(features).filter(feature =>
+      settings[feature] &&
+      (!features[feature].test ||
+      features[feature].test(parsedUrl, document.title || ''))
+    )
+    document.documentElement.setAttribute('data-refined-twitter-lite', enabledFeatures.join(' '))
+  }
 
   document.head.insertAdjacentHTML('beforeend', `
     <style>
-      [data-refined-twitter-lite~="singleColumn"] [data-testid="sidebarColumn"] {
-        display: none;
-      }
-      [data-refined-twitter-lite~="singleColumn"] .rn-1ye8kvj {
-        min-width: 678px;
-      }
+      ${Object.entries(features).map(([feature, data]) =>
+        data.styles.map(rule =>
+          rule.split(',').map(rule =>
+            `[data-refined-twitter-lite~="${feature}"] ${rule.trim()}`
+          ).join(',')
+        ).join('')
+      ).join("\n")}
     </style>
   `)
+
+  document.body.addEventListener('DOMNodeInserted', () => {
+    if (window.location.href !== prevUrl) {
+      setFeatures()
+      prevUrl = window.location.href
+    }
+  })
+  setFeatures()
+
+  // Customize/Save settings API
+
+  window.setRefinedTwitterLiteFeatures = features => {
+    settings = Object.assign(settings, features)
+    localStorage.setItem(storageKey, JSON.stringify(settings))
+    setFeatures()
+  }
+
+  window.addEventListener('beforeunload', () => {
+    setRefinedTwitterLiteFeatures(settings)
+  })
 }())
